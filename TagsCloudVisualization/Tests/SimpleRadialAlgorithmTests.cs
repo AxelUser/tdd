4e96a106a73b4a3bf38301c6ac9c60c5d519d77e
Alexey Maltsev <aamaltsev@skbkontur.ru>
2017-10-11 13:30:08 +0500

    Added simple tests to check algorithm

--- TagsCloudVisualization/Tests/SimpleRadialAlgorithmTests.cs
+++ TagsCloudVisualization/Tests/SimpleRadialAlgorithmTests.cs
@@ -23,6 +23,70 @@ namespace TagsCloudVisualization.Tests
             algorithm = new SimpleRadialAlgorithm(false);
         }
 
+        [Test]
+        public void FindSpaceForRectangle_AddRectangle_ReturnsRectangle()
+        {
+            var center = new Point(25, 25);
+            var maze = new Rectangle(0, 0, center.X * 2, center.Y * 2);
+            var exisingRectangles = new List<Rectangle>();
+
+            var rectangleSize = new Size(50, 50);
+
+            Assert.AreNotEqual(Rectangle.Empty,
+                algorithm.FindSpaceForRectangle(maze, center, exisingRectangles, rectangleSize));
+        }
+
+        [TestCase(30, 50)]
+        [TestCase(50, 30)]
+        public void FindSpaceForRectangle_AddedRectangleTwice_ReturnsTwoRectangles(int rectWidth, int rectHeight)
+        {
+            var center = new Point(rectWidth, rectHeight);
+            var maze = new Rectangle(0, 0, center.X * 2, center.Y * 2);
+
+            var rectangleSize = new Size(rectWidth, rectHeight);
+
+            var firstRectangle = algorithm.FindSpaceForRectangle(maze, center, new List<Rectangle>(), rectangleSize);
+            var secondRectangle =
+                algorithm.FindSpaceForRectangle(maze, center, new List<Rectangle>() {firstRectangle}, rectangleSize);
+            
+            Assert.AreEqual(rectangleSize, firstRectangle.Size);
+            Assert.AreEqual(rectangleSize, secondRectangle.Size);
+        }
+
+        [TestCase(30, 50)]
+        [TestCase(50, 30)]
+        public void FindSpaceForRectangle_AddedRectangleTwice_RectanglesDoesNotOverlap(int rectWidth, int rectHeight)
+        {
+            var center = new Point(rectWidth, rectHeight);
+            var maze = new Rectangle(0, 0, center.X * 2, center.Y * 2);
+
+            var rectangleSize = new Size(rectWidth, rectHeight);
+
+            var firstRectangle = algorithm.FindSpaceForRectangle(maze, center, new List<Rectangle>(), rectangleSize);
+            var secondRectangle =
+                algorithm.FindSpaceForRectangle(maze, center, new List<Rectangle>() { firstRectangle }, rectangleSize);
+
+            Assert.IsFalse(firstRectangle.IntersectsWith(secondRectangle), "Rectangles must not overlap");
+        }
+
+        /// <param name="rectWidth">Must be below or equal 50</param>
+        /// <param name="rectHeight">Must be below or equal 50</param>
+        [TestCase(30, 50)]
+        [TestCase(50, 30)]
+        [TestCase(50, 50)]
+        public void FindSpaceForRectangle_TooMuchRectangles_ReturnsEmptyRectangle(int rectWidth, int rectHeight)
+        {
+            if (rectWidth > 50) throw new ArgumentException("Must be below or equal 50", nameof(rectWidth));
+            if (rectHeight > 50) throw new ArgumentException("Must be below or equal 50", nameof(rectHeight));
+
+            var center = new Point(25, 25);
+            var maze = new Rectangle(0, 0, center.X * 2, center.Y * 2);
+            var rectangleSize = new Size(rectWidth, rectHeight);
+            var exisingRectangles = new List<Rectangle>() {new Rectangle(Point.Empty, rectangleSize)};
+
+            Assert.AreEqual(Rectangle.Empty,
+                algorithm.FindSpaceForRectangle(maze, center, exisingRectangles, rectangleSize));
+        }
 
         [Test, Timeout(100)]
         public void FindSpaceForRectangle_AddBigRectangle_ReturnsEmptyRectangleAtOnce()
